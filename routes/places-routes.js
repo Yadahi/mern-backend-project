3e394b922a2aa5aa49c8df052314cd2358a02481
Viliam <vatvil@gmail.com>
2024-06-06 22:02:50 +0200

    places and users routes

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,8 +2,36 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.json({ message: "This is the places page." });
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/399px-Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+  res.json({ place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const places = DUMMY_PLACES.filter((u) => {
+    return u.creator === userId;
+  });
+  res.json({ places });
 });
 
 module.exports = router;
